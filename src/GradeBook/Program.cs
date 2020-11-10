5344751f9d6aaa30e43046bfba90cfb18f31d795
Aniket Deotale <aniket.deotale98@gmail.com>
2020-11-10 12:23:46 +0530

    This is my First commit to GradeBook

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var book=new Book("Aniket's Grade Book");
+           book.AddGrade(89.1);
+           book.AddGrade(90.5);
+           book.AddGrade(77.5);
+           
+           var stats= book.GetStatistics();
+
+
+           System.Console.WriteLine($"The lowest grades is {stats.Low}");
+            System.Console.WriteLine($"The Gighest grades is {stats.High}");
+            System.Console.WriteLine($"The average is {stats.Average}");
+        }
+    }
+}
---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+           var book=new Book("");
+           book.AddGrade(89.1);
+           book.AddGrade(90.5);
+           book.AddGrade(77.5);
+
+
+           var result = book.GetStatistics();
+
+           Assert.Equal(85.7,result.Average);
+            Assert.Equal(90.5,result.High);
+             Assert.Equal(77.5,result.Low);
+         
+        }
+    }
+}
