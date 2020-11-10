b4c75878fb00b8c561cbc1f7ec1c842af51343fd
Aniket Deotale <aniket.deotale98@gmail.com>
2020-11-10 12:47:22 +0530

    New tests added

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -8,17 +8,43 @@ namespace GradeBook.Tests
         [Fact]
         public void Test1()
         {
-        //   var book1= GetBook("Book 1");
-        //   var book2= GetBook("Book 2");
+          var book1= GetBook("Book 1");
+          SetName(book1,"New Name");
 
-        //   Assert.Equal("Book 1" , book1.name);
-        //   Assert.Equal("Book 2" , book2.name);
+          Assert.Equal("New Name" , book1.Name);
+         
         }
 
-        // Book GetBook(string name)
-        // {
-        //     return new Book(name);
-        // }
+        private void SetName(Book book, string name)
+        {
+            book.Name=name;
+        }
+
+
+        [Fact]
+        public void GetBookReturnDifferentObjects()
+        {
+          var book1= GetBook("Book 1");
+          var book2= GetBook("Book 2");
+
+          Assert.Equal("Book 1" , book1.Name);
+          Assert.Equal("Book 2" , book2.Name);
+          Assert.NotSame(book1,book2);
+        }
+        [Fact]
+         public void TwoVarsCanReferenceSameObject()
+        {
+          var book1= GetBook("Book 1");
+          var book2= book1;
+
+          Assert.Same(book1,book2);
+          Assert.True(Object.ReferenceEquals(book1,book2));
+        }
+
+        Book GetBook(string name)
+        {
+            return new Book(name);
+        }
            
         
     }
